f0e4542011c2596671996ed97c8dad3b5f8ae853
R. Nicolás Savinelli <rsavinelli@est.frba.utn.edu.ar>
2020-09-03 12:29:03 -0300

    user-fetch.js

---
+++ js/user-fetch.js
@@ -0,0 +1,20 @@
+const github = new GitHub;
+
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keypress', (event) => {
+    const userText = event.target.value;
+
+    if (event.key == 'Enter') {
+        if(userText !== '') {
+            github.getUser(userText).then(data => {
+                if(data.profileData.message === 'Not Found') {
+                    console.log(data.profileData.message);
+                } else {
+                    console.log(data.profileData);
+                }
+            })
+        }
+    }
+});
+
